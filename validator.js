934d23e2548c47ab443b07061bb598383b566884
tawanda <tawanda.nyakudjga@gmail.com>
2016-11-15 16:26:02 +0800

    Create validator.js

---
+++ validator.js
@@ -0,0 +1,75 @@
+/*
+* Name : Vue Validator
+* Author : Tawanda Nyakudjga
+* Date : November 2016
+* Description : A bootsrap and jquery form validation library
+**/
+
+var $ = require('jquery');
+
+var vd = module.exports ={
+    addError: (field) => {
+        $(field).closest('.form-group').addClass('has-error');
+        $(field).focus();
+        $(field).select();
+    },
+
+    removeError:(field) => {
+        $(field).closest('.form-group').removeClass('has-error');
+    },
+
+    isNotEmpty:(field) => {
+        var inputStr = $(field).val();
+        $(field).closest('.form-group').removeClass('has-error');
+        if (inputStr == "" || inputStr == null) {
+            vd.addError(field);
+            vd.errors.push("required field");
+            return false;
+        }
+        return true;
+
+    },
+    isNumber:(field) => {
+        if (isNotEmpty(field)) {
+            var inputStr = field.value;
+            vd.removeError(field);
+            for (var i = 0; i < inputStr.length; i++) {
+                var oneChar = inputStr.substring(i, i + 1);
+                if (oneChar < "0" || oneChar > "9") {
+                    vd.addError(field);
+                    vd.errors.push("a number is required");
+                    return false;
+                }
+            }
+            if (field.value.length < 9) {
+                vd.addError(field);
+                vd.errors.push("a number is required");
+                return false;
+            }
+            return true;
+        }
+        return false;
+    },
+
+    validate:(form) => {
+        var fields = $(form).find(':input');
+        $.each(fields, function(i, field) {
+            if ($(field).attr('required')=='required' || $(field).attr('required')=='true') {
+                if (!vd.isNotEmpty(field)) {
+
+                    vd.isValid =false;
+                }
+            }
+
+            if ($(field).attr('number')) {
+                if (!vd.isNumber(field)) {
+
+                    vd.isValid =false;
+                }
+            }
+        });
+        return vd;
+    },
+    errors:Array(),
+    isValid:true
+}
